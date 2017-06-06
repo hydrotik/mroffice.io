d3e47709a8295920f5a2375f5f80873ea433e5b2
Adams <dadams@LM5000026800.local>
2017-06-06 16:34:15 -0400

    adding aqua

--- server.js
+++ server.js
@@ -1,77 +1,15 @@
 'use strict';
-
-const Hapi = require('hapi');
-const Good = require('good');
-const Path = require('path');
-
-// Create a server with a host and port
-const server = new Hapi.Server({
-    connections: {
-        routes: {
-            files: {
-                relativeTo: Path.join(__dirname, 'public')
-            }
-        }
-    }
-});
+const Composer = require('./index');
 
 
-server.connection({ 
-    host: 'localhost', 
-    port: 8000 
-});
-
-server.register({
-    register: Good,
-    options: {
-        reporters: {
-            console: [{
-                module: 'good-squeeze',
-                name: 'Squeeze',
-                args: [{
-                    response: '*',
-                    log: '*'
-                }]
-            }, {
-                module: 'good-console'
-            }, 'stdout']
-        }
-    }
-}, (err) => {
+Composer((err, server) => {
 
     if (err) {
-        throw err; // something bad happened loading the plugin
+        throw err;
     }
 
-    server.start((err) => {
+    server.start(() => {
 
-        if (err) {
-            throw err;
-        }
-        server.log('info', 'Server running at: ' + server.info.uri);
+        console.log('Started the plot device on port ' + server.info.port);
     });
 });
-
-
-// Routes
-
-server.route({
-    method: 'GET',
-    path: '/',
-    handler: function (request, reply) {
-        reply('Hello, Mr. World!');
-    }
-});
-
-
-server.route({
-    method: 'GET',
-    path: '/{name}',
-    handler: function (request, reply) {
-        reply('Hello, Mr. ' + encodeURIComponent(capitalizeFirstLetter(request.params.name)) + '!');
-    }
-});
-
-function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-}
