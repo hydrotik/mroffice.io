cde9297c962140611d18fdc86b4728f09e798618
Adams <dadams@LM5000026800.local>
2017-06-06 15:49:59 -0400

    added Good, Inert

--- server.js
+++ server.js
@@ -1,13 +1,57 @@
 'use strict';
 
 const Hapi = require('hapi');
+const Good = require('good');
+const Path = require('path');
 
 // Create a server with a host and port
-const server = new Hapi.Server();
+const server = new Hapi.Server({
+    connections: {
+        routes: {
+            files: {
+                relativeTo: Path.join(__dirname, 'public')
+            }
+        }
+    }
+});
+
+
 server.connection({ 
     host: 'localhost', 
     port: 8000 
 });
+
+server.register({
+    register: Good,
+    options: {
+        reporters: {
+            console: [{
+                module: 'good-squeeze',
+                name: 'Squeeze',
+                args: [{
+                    response: '*',
+                    log: '*'
+                }]
+            }, {
+                module: 'good-console'
+            }, 'stdout']
+        }
+    }
+}, (err) => {
+
+    if (err) {
+        throw err; // something bad happened loading the plugin
+    }
+
+    server.start((err) => {
+
+        if (err) {
+            throw err;
+        }
+        server.log('info', 'Server running at: ' + server.info.uri);
+    });
+});
+
 
 // Routes
 
@@ -31,12 +75,3 @@ server.route({
 function capitalizeFirstLetter(string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
-
-// Start the server
-server.start((err) => {
-
-    if (err) {
-        throw err;
-    }
-    console.log('Server running at:', server.info.uri);
-});
